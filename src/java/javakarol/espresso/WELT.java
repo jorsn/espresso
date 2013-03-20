e03ef1562e3c043c9c951bd2aeef90b539baa97c
Johannes Rosenberger <jo.rosenberger@gmx-topmail.de>
2013-03-20 22:40:41 +0100

    reconstituted ROBOTER: filled empty file
    	modified:   javakarol/espresso/WELT.java

---
+++ src/java/javakarol/espresso/WELT.java
@@ -0,0 +1,21 @@
+package javakarol.espresso;
+
+import javakarol.Welt;
+
+public class WELT extends Welt {
+
+	public WELT() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public WELT(String weltdatei) {
+		super(weltdatei);
+		// TODO Auto-generated constructor stub
+	}
+
+	public WELT(int breite, int laenge, int hoehe) {
+		super(breite, laenge, hoehe);
+		// TODO Auto-generated constructor stub
+	}
+
+}
